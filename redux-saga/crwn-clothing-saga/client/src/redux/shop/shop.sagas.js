250189d33559db40743015acfb6396e165acbfba
pcsmomo <dongwoo_333@hanmail.net>
2021-10-04 18:03:49 +1100

    198. Redux Thunk Into Saga

--- redux-saga/crwn-clothing-saga/client/src/redux/shop/shop.sagas.js
+++ redux-saga/crwn-clothing-saga/client/src/redux/shop/shop.sagas.js
@@ -1,9 +1,43 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, call, put } from "redux-saga/effects";
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from "./shop.actions";
 
 import ShopActionTypes from "./shop.types";
 
 export function* fetchCollectionsAsync() {
   yield console.log("I am fired");
+
+  /* Saga way */
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    // call : (https://redux-saga.js.org/docs/api/#callfn-args)
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    // put : dispatch (https://redux-saga.js.org/docs/api/#putaction)
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+
+  // /* Promise way - thunk */
+  // collectionRef
+  //   .get()
+  //   .then((snapshot) => {
+  //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+  //     dispatch(fetchCollectionsSuccess(collectionsMap));
+  //   })
+  //   .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
 }
 
 export function* fetchCollectionsStart() {

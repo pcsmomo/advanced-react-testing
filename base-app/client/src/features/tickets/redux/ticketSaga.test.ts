f2ce575c086bfcefb062d8bb2dd94dc2347c2842
pcsmomo <dongwoo_333@hanmail.net>
2021-10-11 07:12:13 +1100

    19. Abstracting cancelTransaction Saga Tests and Network Providers

--- base-app/client/src/features/tickets/redux/ticketSaga.test.ts
+++ base-app/client/src/features/tickets/redux/ticketSaga.test.ts
@@ -13,7 +13,7 @@ import {
 } from 'redux-saga/effects';
 import { expectSaga } from 'redux-saga-test-plan';
 import * as matchers from 'redux-saga-test-plan/matchers';
-import { throwError } from 'redux-saga-test-plan/providers';
+import { StaticProvider, throwError } from 'redux-saga-test-plan/providers';
 
 import { HoldReservation } from '../../../../../shared/types';
 import {
@@ -52,13 +52,23 @@ const holdAction = {
   payload: holdReservation,
 };
 
+const networkProviders: Array<StaticProvider> = [
+  [matchers.call.fn(reserveTicketServerCall), null],
+  [matchers.call.fn(releaseServerCall), null],
+];
+
+test('cancelTransaction cancels hold and resets transaction', () => {
+  return expectSaga(cancelTransaction, holdReservation)
+    .provide(networkProviders)
+    .call(releaseServerCall, holdReservation)
+    .put(resetTransaction())
+    .run();
+});
+
 describe('common to all flows', () => {
   test('starts with hold call to server', () => {
     return expectSaga(ticketFlow, holdAction)
-      .provide([
-        [matchers.call.fn(reserveTicketServerCall), null],
-        [matchers.call.fn(releaseServerCall), null],
-      ])
+      .provide(networkProviders)
       .dispatch(
         startTicketAbort({
           reservation: holdReservation,
@@ -76,12 +86,12 @@ describe('common to all flows', () => {
             matchers.call(reserveTicketServerCall),
             throwError(new Error('it did not work')),
           ],
-          [matchers.call.fn(releaseServerCall), null],
           // write provider for selector
           [
             matchers.select.selector(selectors.getTicketAction),
             TicketAction.hold,
           ],
+          ...networkProviders,
         ])
         // assert on showToast action
         .put(
@@ -89,6 +99,7 @@ describe('common to all flows', () => {
             generateErrorToastOptions('it did not work', TicketAction.hold)
           )
         )
+        .call(cancelTransaction, holdReservation)
         .run()
     );
   });

5f6bb5015ed702359e284a7d46eed81e87ac12bc
pcsmomo <dongwoo_333@hanmail.net>
2021-10-15 07:09:58 +1100

    44. Using Custom render in Tests

--- base-app/client/src/features/auth/components/UserProfile.test.tsx
+++ base-app/client/src/features/auth/components/UserProfile.test.tsx
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-
+import { render, screen } from '../../../test-utils';
 import { UserProfile } from './UserProfile';
 
+const testUser = {
+  email: 'booking@avalancheofcheese.com',
+};
+
 test('greets the user', () => {
+  render(<UserProfile />, {
+    preloadedState: { user: { userDetails: testUser } },
+  });
+  expect(
+    screen.getByText(/hi, booking@avalancheofcheese.com/i)
+  ).toBeInTheDocument();
+});
+
+test('redirects if user is falsy', () => {
   render(<UserProfile />);
-  expect(screen.getByText(/hi/i)).toBeInTheDocument();
+  expect(screen.queryByText(/hi/i)).not.toBeInTheDocument();
 });

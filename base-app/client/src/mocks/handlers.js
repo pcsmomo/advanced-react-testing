c4b52933acc4925319b0a41061c976cec527e970
pcsmomo <dongwoo_333@hanmail.net>
2021-10-16 09:06:55 +1100

    57. Writing Handler for Route with URL Params

--- base-app/client/src/mocks/handlers.js
+++ base-app/client/src/mocks/handlers.js
@@ -1,10 +1,22 @@
 import { rest } from 'msw';
 
+import { bandUrl } from '../features/band/redux/bandApi';
 import { showsUrl } from '../features/tickets/redux/showApi';
-import { shows } from '../test-utils/fake-data';
+import { bands, shows } from '../test-utils/fake-data';
 
 export const handlers = [
-  rest.get(showsUrl, (rea, res, ctx) => {
+  rest.get(showsUrl, (req, res, ctx) => {
     return res(ctx.json({ shows }));
+  }),
+  rest.get(`${bandUrl}/:bandId`, (req, res, ctx) => {
+    const { bandId } = req.params;
+
+    return res(
+      ctx.json({
+        // bandId is conveniently the index in the band array
+        // band: bands[bandId],
+        band: bands.find((band) => band.id === parseInt(bandId, 10)),
+      })
+    );
   }),
 ];

df71877d9ec4325bf3b69371dd46b75dcb3b0e74
pcsmomo <dongwoo_333@hanmail.net>
2021-10-18 08:01:44 +1100

    73. Starting Test for Protected Route Redirect after Sign In

--- base-app/client/src/features/auth/tests/protected-routes.test.tsx
+++ base-app/client/src/features/auth/tests/protected-routes.test.tsx
@@ -1,3 +1,5 @@
+import userEvent from '@testing-library/user-event';
+
 import { App } from '../../../App';
 import { render, screen } from '../../../test-utils';
 
@@ -9,4 +11,25 @@ test.each([
   render(<App />, { routeHistory: [route] });
   const signInHeader = screen.getByRole('heading', { name: /sign/i });
   expect(signInHeader).toBeInTheDocument();
+});
+
+test('successful sign-in flow', () => {
+  // go to protected page
+  const { history } = render(<App />, { routeHistory: ['/tickets/1'] });
+
+  // Sign in (after redirect)
+  const emailField = screen.getByLabelText(/email/i);
+  userEvent.type(emailField, 'booking@avalancheofcheese.com');
+
+  const passwordField = screen.getByLabelText(/password/i);
+  userEvent.type(passwordField, 'iheartcheese');
+
+  const signInButton = screen.getByRole('button', { name: /sign in/i });
+  userEvent.click(signInButton);
+
+  // test redirect back to protected page
+  expect(history.location.pathname).toBe('/tickets/1');
+
+  // sign-in page remove from history
+  console.log(history);
 });

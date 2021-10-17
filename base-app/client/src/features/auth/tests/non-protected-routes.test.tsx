58b95a4be5f4e4055229a7a76550accd7caa9690
pcsmomo <dongwoo_333@hanmail.net>
2021-10-18 07:24:08 +1100

    67. Parametrizing Non-Protected Page Tests with test.each()

--- base-app/client/src/features/auth/tests/non-protected-routes.test.tsx
+++ base-app/client/src/features/auth/tests/non-protected-routes.test.tsx
@@ -1,8 +1,19 @@
 import { App } from '../../../App';
 import { render, screen } from '../../../test-utils';
 
-test('home page does not redirect to login screen', () => {
-  render(<App />); // don't need to specify route
-  const homeHeading = screen.getByRole('heading', { name: /welcome/i });
-  expect(homeHeading).toBeInTheDocument();
-});
+test.each([
+  { routeName: 'Home', routePath: '/', headingMatch: /welcome/i },
+  { routeName: 'Band 1', routePath: '/bands/1', headingMatch: /joyous/i },
+  { routeName: 'Shows', routePath: '/shows', headingMatch: /shows/i },
+])(
+  // can't use object properties in test name until
+  // https://github.com/facebook/jest/pull/11388 is released
+  '$routeName page does not redirect to login screen',
+  async ({ routePath, headingMatch }) => {
+    render(<App />, { routeHistory: [routePath] });
+    const heading = await screen.findByRole('heading', {
+      name: headingMatch,
+    });
+    expect(heading).toBeInTheDocument();
+  }
+);

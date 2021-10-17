cc67aef1719c4dfc8cc88131c9ad0b87fe1cc195
pcsmomo <dongwoo_333@hanmail.net>
2021-10-18 07:32:24 +1100

    70. Parametrizing Sign-In Redirect

--- base-app/client/src/features/auth/tests/protected-routes.test.tsx
+++ base-app/client/src/features/auth/tests/protected-routes.test.tsx
@@ -1,8 +1,12 @@
 import { App } from '../../../App';
 import { render, screen } from '../../../test-utils';
 
-test('redirects to sign-in from /profile when not authenticated', () => {
-  render(<App />, { routeHistory: ['/profile'] });
+test.each([
+  { route: '/profile' },
+  { route: '/tickets/0' },
+  { route: '/confirm/0?holdId=123&seatCount=2' },
+])('redirects to sign-in from $route when not authenticated', ({ route }) => {
+  render(<App />, { routeHistory: [route] });
   const signInHeader = screen.getByRole('heading', { name: /sign/i });
   expect(signInHeader).toBeInTheDocument();
 });
